b20e94ecbe5890a45fd1efd762c58fd10c951049
Akshay <akshaysharma4220@gmail.com>
2026-01-07 00:06:58 +0530

    Add files via upload
    todo app

---
+++ script.js
@@ -0,0 +1,56 @@
+const inputtext = document.getElementById("inputtext")
+const addbutton = document.getElementById("addtask")
+const todolist = document.getElementById("listtodo")
+
+
+
+
+function addtask() {
+    const tasktext = inputtext.value.trim();
+
+
+    if (tasktext == "") {
+        return;
+    }
+    const li = document.createElement('li');
+    li.textContent = tasktext;
+
+
+    li.addEventListener('click', function () {
+        li.classList.toggle("done");
+
+
+
+
+    });
+       
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.className = "deleteBtn";
+
+        // Add delete functionality
+        deleteBtn.addEventListener("click", (e) => {
+              e.stopPropagation();
+            li.remove(); // removes the task
+        });
+
+ li.appendChild(deleteBtn);
+    todolist.appendChild(li);
+
+
+    inputtext.value = '';
+    inputtext.focus();
+
+
+    // Create Delete button
+
+}
+addbutton.addEventListener("click", addtask)
+inputtext.addEventListener('keypress', function (e) {
+    if (e.key == 'Enter') {
+        addtask();
+    }
+})
+window.onload = function () {
+    inputtext.focus();
+};
